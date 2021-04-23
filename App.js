a969267ab02c0ed49eff58026e01649f61e5e379
TWIT <40360709+HackDDOSAttack@users.noreply.github.com>
2021-04-24 04:03:49 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavBar } from "./src/NavBar";
-import {AddTodo} from "./src/AddTodo";
-import {Todo} from "./src/Todo";
+import { Alert, StyleSheet, Text, View, FlatList } from 'react-native';
+import { NavBar } from "./src/components/NavBar";
+import { TodoScreen } from "./src/screens/TodoScreen";
+import { MainScreen } from "./src/screens/MainScreen";
 
 export default function App() {
-const [todos, setTodos] = useState([]);
+  const [todoId, setTodoId] = useState(null);
+  const [todos, setTodos] = useState([
+{id: "1", title: "Боже, как же я"},
+{id: "2", title: "Обожаю"},
+{id: "3", title: "ККРИТ!!!"}
+  ]);
 
 const addTodo = title => {
   setTodos(prev => [
@@ -17,19 +22,68 @@ const addTodo = title => {
   ]);
 };
 
+const removeTodo = id => {
+  const todo = todos.find(t => t.id === id);
+  Alert.alert(
+    "Удаление элемента",
+    'Вы уверены, что хотите удалить $(todo.title)',
+    [
+      {
+        text: "Отмена",
+        style: "cancel"
+      },
+      {
+        text: "Удалить",
+        onPress: () => {
+          setTodoId(null)
+          setTodos(prev => prev.filter(todo => todo.id !== id));
+        }
+      }
+    ],
+    { cancelable: false }
+  );
+};
+
+const updateTodo = (id, title) => {
+  setTodos(old =>
+    old.map(todo => {
+      if (todo.id === id) { 
+        todo.title = title;
+      }
+      return todo;
+    })
+  );
+};
+
+let content = (
+  <MainScreen
+  todos={todos}
+  addTodo={addTodo}
+  removeTodo={removeTodo}
+  openTodo={setTodoId}
+  />
+);
+
+if (todoId)  {
+  const selectedTodo = todos.find(todo => todo.id === todoId);
+  content = (
+    <TodoScreen
+    goBack={() => {
+      setTodoId(null);
+    }}
+    todo={selectedTodo}
+    onRemove={removeTodo}
+    onSave={updateTodo}
+    />
+  );
+}
+
 return (
   <View>
   <NavBar title = "I Love KKRIT. THIS IS THE BEST COLLEGE"/>
-  <View style={styles.container}>
-    <AddTodo onSublit={addTodo} />
-    <View>
-    {todos.map(todo=> (
-      <Todo key={todo.id} todo={todo} />
-    ))}
-</View>
-</View>
-</View>
-);
+  <View style={styles.container}>{content}</View>
+    </View>  
+  );
 }
 
 const styles = StyleSheet.create({
